bf0cfe07dc0f027c9f6d680d03561cd93f807934
Ertman_Matthew <ertmm411@hsd.k12.or.us>
2025-06-10 08:33:02 -0700

    Finally got it working, added link to fxmlcontroller

--- dice-roller/src/main/java/com/example/DiceController.java
+++ dice-roller/src/main/java/com/example/DiceController.java
@@ -1,23 +1,26 @@
 package com.example;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 
-public class DiceController {
+public class DiceController implements Initializable{
 
     @FXML
     private Spinner<Integer> inputOne;
     SpinnerValueFactory<Integer> inputOneValueFactory = 
-        new SpinnerValueFactory.IntegerSpinnerValueFactory(1,128,1);
+        new SpinnerValueFactory.IntegerSpinnerValueFactory(0,128,0);
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) 
     {
         inputOne.setValueFactory(inputOneValueFactory);
         inputOneValueFactory.setValue(0);
+        System.out.println(inputOne.getValue());
     }
             
 }
